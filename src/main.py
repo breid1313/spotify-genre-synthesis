17df34ba3139a82d219f7135fc0447bbb499dd95
Brian Reid <breid1313@gmail.com>
2021-02-22 21:31:16 -0500

    print head and info

--- src/main.py
+++ src/main.py
@@ -9,4 +9,5 @@ DATA_FILE = Path(__file__).parent.parent / "./data/data.csv"
 
 # read the data into a DataFrame 
 tracks = pd.read_csv(DATA_FILE)
-print(df.head())
+print(tracks.head())
+print(tracks.info())
